a6d6887fb91120643cb38334b3f23409e14806d8
Philipp Hirsch <itself@hanspolo.net>
2015-06-23 10:46:18 +0200

    Implement summary of wysiwyg-paragraph.

--- app/models/contentr/wysiwyg_paragraph.rb
+++ app/models/contentr/wysiwyg_paragraph.rb
@@ -1,10 +1,13 @@
 # coding: utf-8
 module Contentr
   class WysiwygParagraph < Paragraph
+    include ActionView::Helpers::SanitizeHelper
+
     field :body, type: 'text'
 
     def summary
-      'FIXME'
+      summary = strip_tags(self.body)[0..100]
+      summary + (self.body.length > 100 ? '...' : '')
     end
 
     def self.cache?
